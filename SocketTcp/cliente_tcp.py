f9a84d3786b3841aeb113e2ec47603c44e1272ed
HenriqueRicardoFigueira <henriquefigueira@alunos.utfpr.edu.br>
2020-08-17 17:53:41 -0300

    add download files

--- SocketTcp/cliente_tcp.py
+++ SocketTcp/cliente_tcp.py
@@ -8,22 +8,46 @@
 
 import socket
 import pickle
+import os
+import tqdm
 
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 
+host = '127.0.0.1'
+port = 5000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 5000))
+print(f"[+] Connecting to {host}:{port}")
+sock.connect((host, port))
+print("[+] Connected.")
 
 while True:
     messag = input("Digite um comando: ")
+    dow = messag.split(" ")
     message = bytes(messag, "utf-8")
     sock.send(message)
     if messag == "exit" or messag == "EXIT":
         sock.close()
         break
+
     elif messag == "files" or messag == "FILES":
-        data = sock.recv(1024)
+        data = sock.recv(BUFFER_SIZE)
         print(pickle.loads(data))
+
+    elif dow[0] == "down" or dow[0] == "DOWN":
+        data = sock.recv(BUFFER_SIZE)
+        filename, filesize = data.decode().split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open("./recive/"+filename, "wb") as f:
+            for _ in progress:
+                bytes_read = sock.recv(BUFFER_SIZE)
+                if not bytes_read:    
+                    break
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
     else:
-        data = sock.recv(1024)
+        data = sock.recv(BUFFER_SIZE)
         if data != b"":
             print(str(repr(data)))
     
--- SocketTcp/server_tcp.py
+++ SocketTcp/server_tcp.py
@@ -6,11 +6,15 @@
 # descrição: Código referente a atividade Programação com sockets TCP
 ###
 
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 
+
 import socket
 from datetime import datetime
 import pickle
 import struct
 import os
+import tqdm
 
 def recive_message(clientsock):
     data = clientsock.recv(1024)
@@ -36,6 +40,7 @@ client, addr = sock.accept()
 while True:
     if client:
         dataRecive = recive_message(client)
+        dow = dataRecive.decode().split(" ")
         print(dataRecive)
         if dataRecive != b'':
             dataDecode = dataRecive.decode()            
@@ -52,7 +57,22 @@ while True:
                 nov = (len(listDir), listDir)
                 dados = pickle.dumps(nov)
                 client.send(dados)
-
+            
+            elif dow[0] == "down" or dow[1] == "DOWN":
+                n = dataDecode.split(" ")
+                filename = str(n[1])
+                filesize = os.path.getsize("./shared/"+filename)
+                data_up = f"{filename}{SEPARATOR}{filesize}".encode()
+                client.send(data_up)
+                progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+                with open("./shared/"+filename, "rb") as f:
+                   for _ in progress:
+                        bytes_read = f.read(BUFFER_SIZE)
+                        if not bytes_read:
+                            break
+                        client.sendall(bytes_read)
+                        progress.update(len(bytes_read))
             elif dataDecode == "exit" or dataDecode == "EXIT":
                 client.close()
+                break
                 
